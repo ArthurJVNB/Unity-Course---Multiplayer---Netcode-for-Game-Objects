62fb3b6d944a757f2ccecfb930e0b75ee6a9daf6
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2024-03-10 13:38:44 -0300

    Exploring simple NetworkVariables (int, struct and FixedString).

--- Assets/Project/Scripts/PlayerNetwork.cs
+++ Assets/Project/Scripts/PlayerNetwork.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -5,20 +6,70 @@ namespace Project
 {
 	public class PlayerNetwork : NetworkBehaviour
 	{
+
 		private const int MaxInputLength = 1;
 		[SerializeField] private float _speed = 10;
 
+		// NOTE: It can only be of value type (cannot use reference type)
+		//private NetworkVariable<int> _randomNumber = new(1, writePerm: NetworkVariableWritePermission.Owner);
+		private NetworkVariable<CustomData> _randomNumber = new(new()
+		{
+			Int = 1,
+			Bool = true,
+		}, writePerm: NetworkVariableWritePermission.Owner);
+
 		private Vector2 _movementInput;
 
-		private void Start()
+		private struct CustomData : INetworkSerializable
 		{
-			if (!IsOwner) enabled = false;
+			public int Int;
+			public bool Bool;
+			public FixedString128Bytes Message;
+
+			public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+			{
+				serializer.SerializeValue(ref Int);
+				serializer.SerializeValue(ref Bool);
+				serializer.SerializeValue(ref Message);
+			}
+		}
+
+		// NOTE: ANY NETWORK OBJECT SHOULD NEVER USE START OR AWAKE. YOU NEED TO USE THIS METHOD BELLOW
+		public override void OnNetworkSpawn()
+		{
+			base.OnNetworkSpawn();
+			_randomNumber.OnValueChanged += RandomNumber_OnValueChanged;
+		}
+
+		private void RandomNumber_OnValueChanged(int previousValue, int newValue)
+		{
+			Debug.Log($"{OwnerClientId}; randomNumber: {newValue}");
+		}
+
+		private void RandomNumber_OnValueChanged(CustomData previousValue, CustomData newValue)
+		{
+			Debug.Log($"{OwnerClientId}; randomNumber: {newValue.Int}; {newValue.Bool}; {newValue.Message}");
 		}
 
 		private void Update()
 		{
+			//if (!IsOwner) enabled = false;
+			if (!IsOwner) return;
+
 			GetInputs();
 			HandleMovement();
+
+			if (Input.GetKeyDown(KeyCode.T))
+			{
+				//_randomNumber.Value = Random.Range(0, 100);
+				_randomNumber.Value = new()
+				{
+					Int = Random.Range(0, 100),
+					Bool = !_randomNumber.Value.Bool,
+					Message = System.DateTime.Now.ToString(),
+				};
+			}
+
 		}
 
 		private void GetInputs()
