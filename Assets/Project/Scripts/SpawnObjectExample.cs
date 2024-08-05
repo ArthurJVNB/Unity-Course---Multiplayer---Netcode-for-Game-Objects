d52531e820c74d162b2f212f81c0d9167db9c446
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2024-08-04 21:46:02 -0300

    Despawn objects.

--- Assets/Project/Scripts/SpawnObjectExample.cs
+++ Assets/Project/Scripts/SpawnObjectExample.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ namespace Project
     public class SpawnObjectExample : NetworkBehaviour
     {
         [SerializeField] private Transform _prefab;
+
+        private Queue<Transform> _spawnedObjects;
 
         private void Start()
         {
@@ -19,6 +22,31 @@ namespace Project
             {
                 TestSpawnServerRpc();
             }
+
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                TestDespawnServerRpc();
+            }
+        }
+
+        /// <summary>
+        /// Despawns spawned objects. OBS: If called directly by the client, it won't work, because the client does not know which objects were spawned.
+        /// </summary>
+        private void TestDespawn()
+        {
+            if (_spawnedObjects == null || _spawnedObjects.Count == 0) return;
+
+            Transform item = _spawnedObjects.Dequeue();
+            if (item.TryGetComponent(out NetworkObject networkObject))
+                networkObject.Despawn();
+
+            Debug.Assert(networkObject, "Despawned object should have NetworkObject");
+        }
+
+        [ServerRpc]
+        private void TestDespawnServerRpc()
+        {
+            TestDespawn();
         }
 
         [ServerRpc]
@@ -27,8 +55,14 @@ namespace Project
             var item = Instantiate(_prefab);
 
             if (item.TryGetComponent(out NetworkObject networkObject))
-                networkObject.Spawn(destroyWithScene);
-            
+            {
+                // networkObject.Spawn(destroyWithScene);
+                networkObject.SpawnWithOwnership(OwnerClientId, destroyWithScene);
+            }
+
+            _spawnedObjects ??= new();
+            _spawnedObjects.Enqueue(item);
+
             Debug.Assert(networkObject, "Prefab should have NetworkObject");
         }
     }
